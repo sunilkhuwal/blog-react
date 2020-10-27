d93097cc6f24302b95d46d5399225dc36efb5893
Sunil Kumar <sunil.khuwal@gmail.com>
2020-10-27 12:20:36 +0530

    OPTIONAL | memoize using lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,20 @@
 import jsonPlaceHolder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
   //console.log(JSON.stringify(response));
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// };
+
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
+
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
