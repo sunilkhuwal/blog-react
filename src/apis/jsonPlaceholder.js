1b1010320b36c4ab371a38383e351bc38ad43cd6
Sunil Kumar <sunil.khuwal@gmail.com>
2020-10-27 12:12:33 +0530

    Added user api

--- src/apis/jsonPlaceholder.js
+++ src/apis/jsonPlaceholder.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const jsonPlaceHolder = axios.create(
-    {
-        baseURL: "https://jsonplaceholder.typicode.com/"
-    }
-);
+const jsonPlaceHolder = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/",
+});
 
 export default jsonPlaceHolder;
---
+++ src/components/UserHeader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownState) => {
+  return { user: state.users.find((user) => user.id === ownState.userId) };
+};
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
